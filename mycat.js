3bcc6339bfcf75f9f1e14a8bbbe9d4b7c5c3234d
Noha Francoise <noha.francoise@gmail.com>
2020-11-18 14:23:37 +0100

    .\mycat.js

--- mycat.js
+++ mycat.js
@@ -1,3 +1,25 @@
-rl.on('line', (input) => {
-    console.log(`Received: ${input}`);
-  });
+const readline = require('readline')
+const fs = require('fs');    
+
+if(process.argv.length==2)  {
+    const rl = readline.createInterface({ 
+    input: process.stdin,
+    output: process.stdout
+    });
+    rl.on('line', (input) =>  {
+        console.log(input)
+    });
+    
+} 
+
+else if(process.argv.length==3){
+const displayFile = (filename) => {
+
+    const read = fs.createReadStream(filename);
+    read.on('data', function(chunk) {
+    
+        console.log(chunk.toString())
+    });
+}
+displayFile(process.argv[2])
+}
